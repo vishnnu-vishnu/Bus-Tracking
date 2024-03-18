4bcb3b76fadca0c5494236de918c368b6207ba8a
sidharths <sidharthsuresh599@gmail.com>
2024-03-18 20:26:13 +0530

    commit

--- AdminApi/urls.py
+++ AdminApi/urls.py
@@ -8,7 +8,7 @@ from rest_framework.routers import DefaultRouter
 router=DefaultRouter()
 router.register("route",views.RouteView,basename="route")
 router.register("ownerview",views.OwnersView,basename="ownerview")
-router.register("passengerview",views.PassengerView,basename="ownerview")
+router.register("passengerview",views.PassengerView,basename="passengerview")
 router.register("bus",views.BusView,basename="bus-list")
 router.register("assignedroutes",views.AssignedRoutesView,basename="assignedroutes")
 
